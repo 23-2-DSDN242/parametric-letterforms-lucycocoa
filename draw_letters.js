20cd2e6907026c05ce19ac1e8b43257e2f0853b2
lucycocoa <140350024+lucycocoa@users.noreply.github.com>
2024-04-24 20:21:41 +1200

    ㅁ

--- draw_letters.js
+++ draw_letters.js
@@ -1,6 +1,7 @@
 /* these are optional special variables which will change the system */
-var systemBackgroundColor = "#acf2e7";
-var systemLineColor = "#000090";
+var systemBackgroundColor = "white";
+// var systemBackgroundColor = "#ebe3da";//beige bg
+var systemLineColor = "black";
 var systemBoxColor = "#C73869";
 
 /* internal constants */
@@ -17,19 +18,65 @@ const strokeColor  = "#0a2d27";
  */
 function drawLetter(letterData) {
   // color/stroke setup
-  stroke(strokeColor);
-  strokeWeight(4);
+  noStroke();
+  
+
 
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50  + letterData["offsetx"];
-  let pos2y = 150 + letterData["offsety"];
+  let posx=50
+  let posy=100
+  let size = letterData["size"];
+  let Rposx = posx  + letterData["offsetx"];
+  let Rposy = posy + letterData["offsety"];
+  let RdegreeStart=letterData["R degree S"]
+  let RdegreeEnd=letterData["R degree E"]
+  let size2 = letterData["Bsize"];
+  let Bposx = posx  + letterData["offset2x"];
+  let Bposy = posy + letterData["offset2y"];
+  let BdegreeStart=letterData["B degree S"]
+  let BdegreeEnd=letterData["B degree E"]
+  let size3 = letterData["Gsize"];
+  let Gposx = posx  + letterData["offset3x"];
+  let Gposy = posy + letterData["offset3y"];
+  let GdegreeStart=letterData["G degree S"]
+  let GdegreeEnd=letterData["G degree E"]
+  let size4 = letterData["Ysize"];
+  let Yposx = posx  + letterData["offset4x"];
+  let Yposy = posy + letterData["offset4y"];
+  let YdegreeStart=letterData["Y degree S"]
+  let YdegreeEnd=letterData["Y degree E"]
+  
+  
 
   // draw two circles
-  fill(darkGreen);
-  ellipse(50, 150, 75, 75);
-  fill(lightGreen);
-  ellipse(pos2x, pos2y, size2, size2);
+  angleMode(DEGREES)
+  // fill(darkGreen);
+  // ellipse(50, 150, 75, 75);
+  // fill(lightGreen);
+  // ellipse(Rposx, Rposy, size2, size2);
+
+  
+  strokeWeight(20)
+  noFill();
+  // stroke(216,47,32,220) //red
+  // arc(Rposx,Rposy,size,size,RdegreeStart,RdegreeEnd)
+  // stroke(40,39,126,220) //blue
+  // arc(Bposx,Bposy,size2,size2,BdegreeStart,BdegreeEnd)
+  // stroke(48,111,70,220) //green
+  // arc(Gposx,Gposy,size3,size3,GdegreeStart,GdegreeEnd)
+  // stroke(233,169,59,220) //yellow
+  // arc(Yposx,Yposy,size4,size4,YdegreeStart,YdegreeEnd)
+  stroke(216,47,32,200) //red
+  arc(Rposx,Rposy,size,size,RdegreeStart,RdegreeEnd)
+  stroke(40,39,126,200) //blue
+  arc(Bposx,Bposy,size2,size2,BdegreeStart,BdegreeEnd)
+  stroke(48,111,70,200) //green
+  arc(Gposx,Gposy,size3,size3,GdegreeStart,GdegreeEnd)
+  stroke(233,169,59,200) //yellow
+  arc(Yposx,Yposy,size4,size4,YdegreeStart,YdegreeEnd)
+  
+
+
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
--- sketch.js
+++ sketch.js
@@ -13,21 +13,32 @@ const canvasHeight = 500;
  */
 
 const letterA = {
+  "size1": 150,
   "size": 80,
   "offsetx": 0,
-  "offsety": 35
+  "offsety": 0,
+  "offsetxellipse": 10,
+  "offsetyellipse": 80
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "size": 70,
+  "offsetx": -60,
+  "offsety": 0,
+  "offsetx3": -20,
+  "offsety3": -20,
+  "sizeb":90,
+  "offsetxellipse": -51,
+  "offsetyellipse": -80
 }
 
 const letterC = {
+  "size1": 150,
   "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "offsetx": 0,
+  "offsety": 280,
+  "offsetxellipse": 0,
+  "offsetyellipse": 0
 }
 
 // const backgroundColor  = "#acf2e7";
@@ -67,9 +78,22 @@ function draw () {
 
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
+  let size1 = letterData["size1"];
   let size2 = letterData["size"];
+  let size3 = letterData["sizeb"];
+
   let pos2x = posx + letterData["offsetx"];
   let pos2y = posy + letterData["offsety"];
+  let pos3x = posx + letterData["offsetx3"];
+  let pos3y = posy + letterData["offsety3"];
+  let pos4x = posx + letterData["offsetxellipse"];
+  let pos4y = posy + letterData["offsetyellipse"];
+  let pos5x = posx + letterData["offsetxellipse"];
+  let pos5y = posy + letterData["offsetyellipse"];
+  
+  
+  
+
 
   // draw two circles
  
@@ -77,32 +101,47 @@ function drawLetter(posx, posy, letterData) {
 
 
   // ellipse(posx, posy, 150, 150);
+
+  
   // fill(lightGreen);
  
   // ellipse(pos2x+10, pos2y-35, size2*1.2, size2*1.2);
   // rect(posx+40, posy-80, 20,160)
   // rect(posx+60, posy-80, 20,160)
-  noStroke()
+
+  //////////
+  // noStroke()
+
+  // line(posx+40,posy-80,posx+20,posy-80)
+  // line(posx+40,posy-90,posx+20,posy-90)
 
   stroke("pink")
-  strokeWeight(8)
+  strokeWeight(50)
   noFill()
 
   angleMode(DEGREES)
-  arc(50, 50, 80, 80, 90, 270)
+  arc(posx, posy, size1*1, size1*1, 90, 270)
 
-  arc(140,20,30,30,270,90)
-  arc(140,69,50,50,270,90)
+ 
 
-  arc(250, 50, 80, 80, 90, 270)
+
 
   stroke("skyblue")
-  line(60,5,60,95)
-  line(70,5,70,95)
-  line(130,5,130,95)
-  line(120,5,120,95)
+  line(pos2x+10,pos2y-80,pos2x+10,pos2y+80)
+  // line(pos2x+20,pos2y-80,pos2x+20,pos2y+80)
+  
+  stroke("pink")
+  arc(pos3x+10,pos3y-20,size2*1,size2*1,270,90)
+  arc(pos3x+10,pos3y+50,size3*1,size3*1,270,90)
 
+  noStroke()
+  fill('white')
+  ellipse(pos4x,pos4y,40,40)
 
+  fill('skyblue')
+  ellipse(pos5x,pos5y,20,20)
+
+ 
 
 
 }
